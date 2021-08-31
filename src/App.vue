ccc00d7b64b584b8c6233f9b2acfeb9e63dafdd6
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-08-30 21:20:14 -0300

    Criando o formulário

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container grid-xs py-2">
+      <div>TO DO</div>
+      <form>
+      <div class="input-group">
+        <input type="text" class="form-input" placeholder="Novo todo">
+        <button class="btn btn-primary input-group-btn">Adicionar</button>
+      </div>
+    </form>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'App', 
+  data(){ //ESTADO DOS COMPONENTES
+    return { todos:[] }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'spectre.css/dist/spectre.min.css'
+import 'spectre.css/dist/spectre-icons.min.css'
+
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+//Arquivo onde é instanciado o vue
+// e rederizado a entrada
